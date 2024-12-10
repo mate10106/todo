eb1c4289bb1681302a86074349349fb621fb31e8
Máté Brösztl <brosztl.mate@gmail.com>
2024-12-10 17:33:15 +0100

    Refactor Profile Page and Profile Components for Improved Layout and Styling
    - Adjusted the height of the profile page section to a minimum of 85vh for better visual balance.
    - Refactored the form layout in ProfileComponents to enhance readability and maintainability, while preserving existing functionality.
    
    These changes aim to improve the user experience and maintain a clean code structure.

--- app/(protected)/dashboard/profile/page.tsx
+++ app/(protected)/dashboard/profile/page.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react";
 
 const ProfilePage = () => {
   return (
-    <section className="flex items-center justify-center min-h-screen">
+    <section className="flex items-center justify-center min-h-[85vh]">
       <SessionProvider>
         <ProfileComponents />
       </SessionProvider>
--- components/ProfileComponents.tsx
+++ components/ProfileComponents.tsx
@@ -6,34 +6,32 @@ const ProfileComponents = () => {
   const { data: session } = useSession();
 
   return (
-    <form className="">
-      <div className="flex flex-col mx-auto w-[300px] gap-8">
-        <div className="flex gap-8 justify-end items-center">
-          <label>Name:</label>
-          <input
-            type="text"
-            className="rounded-lg p-2 bg-transparent border border-neutral-700"
-            placeholder={session?.user?.name || "Your name"}
-            readOnly
-          />
-        </div>
-        <div className="flex gap-8 justify-end items-center">
-          <label>Email:</label>
-          <input
-            type="email"
-            className="rounded-lg p-2 bg-transparent border border-neutral-700"
-            placeholder={session?.user?.email || "Your email"}
-            readOnly
-          />
-        </div>
-        <div className="flex gap-8 justify-center items-center">
-          <button className="border p-2 rounded-lg text-sm font-bold">
-            Change password
-          </button>
-          <button className="border p-2 rounded-lg text-sm bg-red-900 font-bold">
-            Delete account
-          </button>
-        </div>
+    <form className="flex flex-col w-[300px] gap-8">
+      <div className="flex gap-8 justify-end items-center">
+        <label>Name:</label>
+        <input
+          type="text"
+          className="rounded-lg p-2 bg-transparent border border-neutral-700"
+          placeholder={session?.user?.name || "Your name"}
+          readOnly
+        />
+      </div>
+      <div className="flex gap-8 justify-end items-center">
+        <label>Email:</label>
+        <input
+          type="email"
+          className="rounded-lg p-2 bg-transparent border border-neutral-700"
+          placeholder={session?.user?.email || "Your email"}
+          readOnly
+        />
+      </div>
+      <div className="flex gap-8 justify-center items-center">
+        <button className="border p-2 rounded-lg text-sm font-bold">
+          Change password
+        </button>
+        <button className="border p-2 rounded-lg text-sm bg-red-900 font-bold">
+          Delete account
+        </button>
       </div>
     </form>
   );
