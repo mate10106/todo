569e2811b7c92cc676987f98b5456e4df45a4b2e
Máté Brösztl <brosztl.mate@gmail.com>
2025-01-06 18:45:53 +0100

    Refactor MobileNavbar for improved structure and accessibility
    - Replaced the unordered list with a div for better semantic structure.
    - Updated navigation links to use the Link component directly within the MobileNavbar, enhancing clarity and accessibility.
    - Maintained existing styles and hover effects for a consistent user experience.
    
    These changes aim to streamline the mobile navigation component and improve code readability.

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -7,15 +7,21 @@ const MobileNavbar = () => {
   return (
     <div className="flex items-center justify-center sm:hidden">
       <div className="w-full max-w-lg rounded-2xl">
-        <ul className="flex flex-col mx-auto ml-2">
-          <li className="flex cursor-pointer gap-4 p-2 text-neutral-600 font-bold text-base hover:bg-slate-500/10 transition-colors  rounded-lg duration-500">
+        <div className="flex flex-col mx-auto ml-2">
+          <Link
+            href="/dashboard/status/today"
+            className="flex cursor-pointer gap-4 p-2 text-neutral-600 font-bold text-base hover:bg-slate-500/10 transition-colors  rounded-lg duration-500"
+          >
             <Home size={22} />
-            <Link href="/dashboard/status/today">Home</Link>
-          </li>
-          <li className="flex cursor-pointer gap-4 p-2 text-neutral-600 font-bold text-base hover:bg-slate-500/10 transition-colors  rounded-lg duration-500">
+            Home
+          </Link>
+          <Link
+            href="/dashboard/profile"
+            className="flex cursor-pointer gap-4 p-2 text-neutral-600 font-bold text-base hover:bg-slate-500/10 transition-colors  rounded-lg duration-500"
+          >
             <User size={22} />
-            <Link href="/dashboard/profile">Profile</Link>
-          </li>
+            Profile
+          </Link>
           <li className="flex cursor-pointer">
             <button
               className="flex items-center w-full gap-4 p-2 text-neutral-600 font-bold text-base hover:bg-slate-500/10 transition-colors  rounded-lg duration-500"
@@ -30,7 +36,7 @@ const MobileNavbar = () => {
               Logout
             </button>
           </li>
-        </ul>
+        </div>
       </div>
     </div>
   );
